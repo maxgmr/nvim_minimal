0f6270ac1be12f6643d6febc197d375c902d3714
Max Gilmour <mgilmour@maxgmr.ca>
2025-01-07 17:03:25 -0800

    disable tree icons

--- lua/maxgmr/nvimtree.lua
+++ lua/maxgmr/nvimtree.lua
@@ -86,6 +86,18 @@ nvim_tree.setup({
 	renderer = {
 		highlight_git = true,
 		root_folder_modifier = ":t",
+		icons = {
+			show = {
+				file = false,
+				folder = false,
+				folder_arrow = false,
+				git = false,
+				modified = false,
+				hidden = false,
+				diagnostics = false,
+				bookmarks = false,
+			},
+		},
 	},
 	on_attach = nvim_tree_on_attach,
 })
